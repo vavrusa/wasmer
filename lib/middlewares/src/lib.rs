debd208ef33eb5d106e0d4d5fd3881fdb627ef24
Marek Vavruša <mvavrusa@cloudflare.com>
2021-07-15 15:12:16 -0700

    wasmer-middlewares: make compiler middleware a feature
    The module exports only `get_remaining_points` and `set_remaining_points`
    for headless use.

--- lib/middlewares/src/lib.rs
+++ lib/middlewares/src/lib.rs
@@ -3,4 +3,5 @@ pub mod metering;
 // The most commonly used symbol are exported at top level of the
 // module. Others are available via modules,
 // e.g. `wasmer_middlewares::metering::get_remaining_points`
+#[cfg(feature = "compiler")]
 pub use metering::Metering;
